d8222760e98b228333ba0d5fd7bf80cce8c98955
Matyas Franc <franc.matyas@gmail.com>
2022-03-11 19:33:29 +0100

    scroll down and up, selection color

--- src/script.js
+++ src/script.js
@@ -1,7 +1,12 @@
-$("#scroll-top").click(function() {
-    window.scroll({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
+$(document).ready(function() {
+    $("#scroll-top").click(function() {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        })
+    })
+    $(".arrow-down").click(function() {
+        document.getElementById("scroll-to").scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"})
     })
 })
